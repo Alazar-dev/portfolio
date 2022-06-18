47533e374290d133ab3b2cc1a877684b01e180e6
Alazar-dev <mealazartadesse@gmail.com>
2022-06-18 22:57:28 +0300

    reusable button

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,11 @@
 import Logo from "../assets/logo.jpeg";
+import { Button } from "../components/common/Buttons";
 
 export default function Header() {
   return (
     <div className="flex justify-between">
       <img className="App-logo" src={Logo} alt="" />
-      <button className="text-white border rounded-lg px-4">Resume</button>
+      <Button url="#" title="Resume" />
     </div>
   );
 }
