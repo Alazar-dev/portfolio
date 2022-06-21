6440575f8226666486b22edc677008e66dea8e3e
Alazar-dev <mealazartadesse@gmail.com>
2022-06-21 05:59:47 +0300

    about section

---
+++ src/components/About.js
@@ -0,0 +1,34 @@
+export default function About({ technologies, profile_pic }) {
+  return (
+    <div className="flex flex-col align-start my-10">
+      <h1 className="text-white text-2xl font-bold pb-4">About Me</h1>
+      <div className="flex">
+        <div>
+          <p className="text-gray-500">
+            I'm an enthusiast Software Engineer having a great passion for
+            developing engineering solutions.
+          </p>
+
+          <p className="text-gray-500">
+            An aspiring and driven Software Engineer with robust problem-solving
+            skills and experience in developing software projects. I am looking
+            forward to work with fast-growing company so that I can make an
+            impact. I bring 2+ years of experience in Full Stack Development and
+            3+ years of experience in programming.
+          </p>
+          <p className="text-white  py-4">
+            Technologies I've been working with:
+          </p>
+          {technologies.map((technology) => (
+            <div>
+              <p className="text-gray-500">{technology.name}</p>
+            </div>
+          ))}
+        </div>
+        <div className="pp_container">
+          <img src={profile_pic} alt="profile" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Header from "../layouts/Header";
 import Hero from "../components/Hero";
+import About from "../components/About";
+import Profile_pic from "../assets/my_pic.png";
 
 export default function Home() {
   return (
@@ -9,6 +11,16 @@ export default function Home() {
         name="Alazar Tadesse"
         profession="Full Stack Developer"
         description="I'm a software engineer based in Addis Ababa, Bsc in Software Engineering from Addis Ababa Institute of Technology."
+      />
+      <About
+        technologies={[
+          { name: "JavaScript (ES6+)" },
+          { name: "React" },
+          { name: "React Native" },
+          { name: "Node.js" },
+          { name: "Laravel" },
+        ]}
+        profile_pic={Profile_pic}
       />
     </div>
   );
