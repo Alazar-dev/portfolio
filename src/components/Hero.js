0c7c03f163a0b60e71ea3b4d62b27907d26b5336
Alazar-dev <mealazartadesse@gmail.com>
2022-06-21 06:52:33 +0300

    improved hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,13 +2,13 @@ import { Button } from "./common/Buttons";
 
 export default function Hero({ name, profession, description }) {
   return (
-    <div className="flex flex-col items-start">
+    <div className="flex flex-col items-start mt-20">
       <p style={{ fontSize: "60px" }} className="text-xl text-white">
         {name}
       </p>
-      <h2 style={{ fontSize: "50px" }}>A {profession}</h2>
-      <p>{description}</p>
-        <Button url="mailto:mealazartadesse@gmail" title="Get In Touch" />
+      <h2 className="text-3xl mt-4 text-white">{profession}</h2>
+      <p className="text-gray-500 mt-2 mb-6">{description}</p>
+      <Button url="mailto:mealazartadesse@gmail" title="Get In Touch" />
     </div>
   );
 }
