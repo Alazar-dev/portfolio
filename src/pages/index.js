307e605bbc89bfec99b02e2058f026bcc66d00f4
Alazar Tadesse <69623057+Alazar-dev@users.noreply.github.com>
2022-08-02 10:31:37 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Hero
         name="Alazar Tadesse"
         profession="Full Stack Developer"
-        description="I'm a software engineer based in Addis Ababa, Bsc in Software Engineering from Addis Ababa Institute of Technology."
+        description="I'm a software engineer based in Addis Ababa, Ehtiopia"
       />
       <About
         technologies={[
