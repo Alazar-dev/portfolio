2e1e9b1fdb012ce944d6f6c767571465ac3c3541
Alazar-dev <mealazartadesse@gmail.com>
2022-09-22 11:16:19 +0300

    update experience

--- src/components/Expreience.js
+++ src/components/Expreience.js
@@ -2,9 +2,9 @@ export default function Experience() {
   return (
     <div className="container">
       <div className="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
-        <div className="flex flex-row-reverse md:contents">
+        <div className="flex md:contents">
           <div className=" col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-            <h3 className="font-semibold text-lg mb-3">2F Capital</h3>
+            <h3 className="font-semibold text-lg mb-3">Awura Computing PLC</h3>
             <div className="flex">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +21,7 @@ export default function Experience() {
                 />
               </svg>
               <p className="text-gray-500 leading-tight text-justify">
-                Full Stack Developer using React.js, Material UI and Node.js
+                I am building responsive layouts using React.js and Material UI.
               </p>
             </div>
             <div className="flex">
@@ -40,8 +40,8 @@ export default function Experience() {
                 />
               </svg>
               <p className="text-gray-500 leading-tight text-justify">
-                Worked on different client websites including Ride Hailing
-                Technology and inventory systems.
+                Implement React best practices to build truly reusable
+                components.
               </p>
             </div>
           </div>
@@ -60,71 +60,7 @@ export default function Experience() {
             <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full  shadow" />
           </div>
           <div className=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
-            <h3 className="font-semibold text-lg mb-3">Tech 1 Factory</h3>
-            <div className="flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
-              <p className="text-gray-500 leading-tight text-justify">
-                Full Stack Developer using React.js, React Native, Node.js,
-                Serverless API, aws(s3, EC2) and Node.js
-              </p>
-            </div>
-            <div className="flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
-              <p className="text-gray-500 leading-tight text-justify">
-                Worked on implementing advanced, responsive and SEO enabled
-                UI/Ux with high quality and code reusability.
-              </p>
-            </div>
-            <div className="flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
-              <p className="text-gray-500 leading-tight text-justify">
-                Built and maintained React.js and React Native projects.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-row-reverse md:contents">
-          <div className=" col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-            <h3 className="font-semibold text-lg mb-3">Bitapps Technologies</h3>
+            <h3 className="font-semibold text-lg mb-3">Bitapps Technology</h3>
             <div className="flex">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -204,11 +140,127 @@ export default function Experience() {
               </p>
             </div>
           </div>
+        </div>
+
+        <div className="flex flex-row-reverse md:contents">
+          <div className=" col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
+            <h3 className="font-semibold text-lg mb-3">Tech1 Factory</h3>
+            <div className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+              <p className="text-gray-500 leading-tight text-justify">
+                Full Stack Developer using React.js, React Native, Node.js,
+                Serverless API, aws(s3, EC2) and Node.js
+              </p>
+            </div>
+            <div className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+              <p className="text-gray-500 leading-tight text-justify">
+                Worked on implementing advanced, responsive and SEO enabled
+                UI/Ux with high quality and code reusability.
+              </p>
+            </div>
+            <div className="flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+              <p className="text-gray-500 leading-tight text-justify">
+                Built and maintained React.js and React Native projects.
+              </p>
+            </div>
+          </div>
+
           <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
             <div className="h-full w-6 flex items-center justify-center">
               <div className="h-full w-1 bg-blue-800 pointer-events-none" />
             </div>
             <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full  shadow" />
+          </div>
+          <div className="flex md:contents">
+            <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+              <div className="h-full w-6 flex items-center justify-center">
+                <div className="h-full w-1 bg-blue-800 pointer-events-none" />
+              </div>
+              <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full  shadow" />
+            </div>
+            <div className=" col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
+              <h3 className="font-semibold text-lg mb-3">2F Capital</h3>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+                <p className="text-gray-500 leading-tight text-justify">
+                  Full Stack Developer using React.js, Material UI and Node.js
+                </p>
+              </div>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+                <p className="text-gray-500 leading-tight text-justify">
+                  Worked on different client websites including Ride Hailing
+                  Technology and inventory systems.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,4 +1,5 @@
 import Logo from "../assets/logo.jpeg";
+import Resume from "../assets/Alazar Tadesse.pdf";
 
 export default function Header() {
   return (
@@ -6,7 +7,7 @@ export default function Header() {
       <img className="App-logo" src={Logo} alt="" />
       <a
         className="text-white border rounded-lg p-2"
-        href="../assets/Alazar%20Tadesse.pdf"
+        href={Resume}
         download="Alazar Tadesse"
       >
         Resume
